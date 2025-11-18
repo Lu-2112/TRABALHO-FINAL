1c1f6d5cca2bd7743fa489653cb8b48d98554260
commit-ana <ana.lima25@iesb.edu.br>
2025-11-17 21:51:01 -0300

    Criando e configurando os arquivos controllers.js: Livro, Fornecedor e Estoque.

---
+++ src/controllers/FornecedorController.js
@@ -0,0 +1,57 @@
+// FORNECEDOR CONTROLLER 
+const express = require('express');
+const router = express.Router();
+
+// Importo o modelo
+const FornecedorModel = require('../models/FornecedorModel');
+
+// Importo os validadores
+const { validarNovoFornecedor, validarAtualizacaoFornecedor } = require('../validators/FornecedorValidator');
+
+// Rotas
+// Leitura
+router.get('/fornecedores', async (req, res, next) => {
+    const fornecedores = await FornecedorModel.find();
+    res.json(fornecedores);
+});
+
+// Leitura por ID
+router.get('/fornecedores/:id', async (req, res, next) => {
+    const fornecedorEncontrado = await FornecedorModel.findById(req.params.id);
+    if (!fornecedorEncontrado) {
+        return res.status(404).json({ erro: 'Fornecedor não encontrado' });
+    }
+    res.status(200).json(fornecedorEncontrado);
+});
+
+// Cadastro
+router.post('/fornecedores', validarNovoFornecedor, async (req, res, next) => {
+    const dados = req.body;
+    const fornecedorCadastrado = await FornecedorModel.create(dados);
+    res.status(201).json(fornecedorCadastrado);
+});
+
+// Atualização
+router.put('/fornecedores/:id', validarAtualizacaoFornecedor, async (req, res, next) => {
+    const id = req.params.id;
+    const novosDados = req.body;
+
+    const fornecedorAtualizado = await FornecedorModel.findByIdAndUpdate(id, novosDados, { new: true });
+
+    if (!fornecedorAtualizado) {
+        return res.status(404).json({ erro: 'Fornecedor não encontrado!' });
+    }
+
+    res.json(fornecedorAtualizado);
+});
+
+// Exclusão
+router.delete('/fornecedores/:id', async (req, res, next) => {
+    const id = req.params.id
+    await FornecedorModel.findByIdAndDelete(id);
+    res.status(204).send("Fornecedor excluído com sucesso!");
+});
+
+
+// Importando o módulo
+module.exports = router;
--- src/controllers/LivroController.js
+++ src/controllers/LivroController.js
@@ -1,1 +1,54 @@
-// LIVRO
+// LIVRO CoNTROLLER
+const express = require('express');
+const router = express.Router();
+
+//Importo o modelo
+const LivrosModel = require ('../models/LivroModels');
+
+//Importo os validadores
+const { validarNovoLivro, validarAtualizacaoLivro } = require ('../validators/LivroValidator');
+
+//Rotas
+//Cadastro
+//Leitura
+router.get('/livros', async (req, res, next) => {
+    const livros = await LivrosModel.find();
+    res.json(livros);
+});
+
+//Leitura por ID
+router .get('/livros/:id', async (req, res, next) => {
+ const livroEncontrado = await LivrosModel.findById(req.params.id);
+    if(!livroEncontrado) {
+        return res.status(404).json({erro: 'Livro não encontrado'});
+    }
+    res.status(200).json(livroEncontrado);
+});
+
+//Cadastro
+router.post('/livros', validarNovoLivro, async (req, res, next) => {
+    const dados = req.body;
+    const livroCadastrado = await LivrosModel.create(dados)
+    res.status(201).json(livroCadastrado)
+});
+
+//Atualização
+router.put('/livros/:id', validarAtualizacaoLivro, async (req, res, next) => {
+    const id = req.params.id
+    const novosDados = req.body
+    const livroAtualizado = await LivrosModel.findByIdAndUpdate(id, novosDados, {new : true}); 
+    if(!livroAtualizado) {
+        return res.status(404).json({erro: 'Livro não encontrado!'});
+    }
+    res.json(livroAtualizado)
+});
+
+//Exclusão
+router.delete('/livros/:id', async (req, res, next) => {
+    const id = req.params.id
+    await LivrosModel.findByIdAndDelete(id);
+    res.status(204).json({mensagem: 'Livro excluído com sucesso!'});
+})
+
+//IMPORTANDO O MÓDULO
+module.exports = router;
